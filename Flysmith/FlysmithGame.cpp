c557bee019a3ccb5cc61e8164db7d0773b998da7
George Cucu <cogloch@outlook.com>
2015-09-04 21:34:12 +0300

    Added basic fuselage geometry generation

--- Flysmith/FlysmithGame.cpp
+++ Flysmith/FlysmithGame.cpp
@@ -6,6 +6,7 @@
 #include "Wing.h"
 #include "Plane.h"
 #include "Entity.h"
+#include "Fuselage.h"
 using namespace DirectX;
 
 
@@ -16,10 +17,11 @@ FlysmithGame::FlysmithGame(HINSTANCE hInstance)
 	LoadResources();
 
 	auto wingMesh = m_resources.GetHandle("Wing");
+	auto planeMesh = m_resources.GetHandle("Plane");
 	U32 vert = m_resources.GetHandle("TestVS");
 	U32 pixel = m_resources.GetHandle("TestPS");
 
-	auto comp0 = m_scene.CreateRenderComponent(wingMesh, vert, pixel),
+	/*auto comp0 = m_scene.CreateRenderComponent(wingMesh, vert, pixel),
 		 comp1 = m_scene.CreateRenderComponent(wingMesh, vert, pixel),
 		 comp2 = m_scene.CreateRenderComponent(wingMesh, vert, pixel);
 	
@@ -30,8 +32,15 @@ FlysmithGame::FlysmithGame(HINSTANCE hInstance)
 	m_scene.entities[1].AttachComponent(comp1, &m_scene.renderComponents[comp1.index]);
 
 	m_scene.CreateEntity(Transform({ 0.0f, 4.0f, 0.0f }), 1);
-	m_scene.entities[2].AttachComponent(comp2, &m_scene.renderComponents[comp2.index]);
+	m_scene.entities[2].AttachComponent(comp2, &m_scene.renderComponents[comp2.index]);*/
+	auto renderComp = m_scene.CreateRenderComponent(planeMesh, vert, pixel);
+	m_scene.CreateEntity();
+	m_scene.entities[0].AttachComponent(renderComp, &m_scene.renderComponents[renderComp.index]);
 
+	auto fuseComp = m_scene.CreateRenderComponent(m_resources.GetHandle("Fuselage"), vert, pixel);
+	m_scene.CreateEntity();
+	m_scene.entities[1].AttachComponent(fuseComp, &m_scene.renderComponents[fuseComp.index]);
+	m_scene.entities[1].pSceneNode->transform.SetRotation(0.0f, XM_PIDIV2, 0.0f);
 }
 
 void FlysmithGame::HandleEvent(const Event& ev)
@@ -59,6 +68,10 @@ void FlysmithGame::LoadResources()
 	plane.AddWingPair(wing, MetersToDXUnits(0), MetersToDXUnits(1.5f), MetersToDXUnits(2));
 	auto planeMesh = plane.GetMesh();
 	m_resources.AddResource("Plane", m_pRenderer->CacheMesh(planeMesh.verts, planeMesh.indices));
+
+	Fuselage fuselage;
+	auto fuselageMesh = fuselage.GenerateMesh();
+	m_resources.AddResource("Fuselage", m_pRenderer->CacheMesh(fuselageMesh.verts, fuselageMesh.indices));
 }
 
 void FlysmithGame::UpdateScene(float dt)
---
+++ Flysmith/Fuselage.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "Mesh.h"
+
+
+struct FuselageRing
+{
+	enum Shape
+	{
+		CIRCULAR
+	};
+};
+
+struct Fuselage
+{
+	Mesh GenerateMesh()
+	{
+		U32 numRings = 4;
+
+		// Generate vertex data
+		Mesh mesh;
+		auto ring3 = GenerateCircularRing(0.1f, { 0.0f, 0.0f, -1.2f });
+		auto ring0 = GenerateCircularRing(0.5f, { 0.0f, 0.0f, -1.0f });
+		auto ring1 = GenerateCircularRing(1.0f, { 0.0f, 0.0f, 0.0f });
+		auto ring2 = GenerateCircularRing(1.0f, { 0.0f, 0.0f, 2.0f });
+		
+		mesh.verts.insert(mesh.verts.end(), ring3.begin(), ring3.end());
+		mesh.verts.insert(mesh.verts.end(), ring0.begin(), ring0.end());
+		mesh.verts.insert(mesh.verts.end(), ring1.begin(), ring1.end());
+		mesh.verts.insert(mesh.verts.end(), ring2.begin(), ring2.end());
+
+		// *Stable* sort by z coord
+
+		// Stitch
+		for (U32 ringIndex = 0; ringIndex < numRings - 1; ringIndex++)
+		{
+			auto numPoints = mesh.verts.size() / numRings;
+			auto ringOffset1 = numPoints * ringIndex;
+			auto ringOffset2 = numPoints * (ringIndex + 1);
+
+			for (U32 i = 0; i < numPoints - 1; i++)
+			{
+				mesh.indices.push_back(ringOffset1 + i);
+				mesh.indices.push_back(ringOffset2 + i + 1);
+				mesh.indices.push_back(ringOffset2 + i);
+			}
+
+			for (U32 i = 0; i < numPoints - 1; i++)
+			{
+				mesh.indices.push_back(ringOffset1 + i);
+				mesh.indices.push_back(ringOffset1 + i + 1);
+				mesh.indices.push_back(ringOffset2 + i + 1);
+			}
+		}
+
+		mesh.GenerateNormals();
+
+		return mesh;
+	}
+
+	// TODO: Change to displacement from center
+	std::vector<Vertex> GenerateCircularRing(F32 diameter, XMFLOAT3 dispFromFront)
+	{
+		F32 radius = diameter / 2.0f;
+
+		std::vector<Vertex> verts;
+
+		for (F32 angle = 0; angle <= XM_2PI; angle += 0.1f)
+		{
+			XMFLOAT3 pos(radius * cos(angle), 
+						 radius * sin(angle) + dispFromFront.y,
+						 dispFromFront.z);
+			XMFLOAT3 normal(0.0f, 0.0f, 0.0f);
+			verts.push_back({ pos, normal });
+		}
+
+		return verts;
+	}
+};
